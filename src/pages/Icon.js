401763ccd31e9454d74de9c8ce790ef2f4950891
junkyum <junkyum884@naver.com>
2020-09-01 16:29:44 +0900

    jk 09011629

--- src/pages/Icon.js
+++ src/pages/Icon.js
@@ -1,10 +1,31 @@
 import React from 'react';
-
+import { ReactSVG } from 'react-svg'
 
 const Icon = () => {
     return (
-        <div>
-            Icon
+        <div className = "Container">
+            <div className = "Tag">
+                <div className = "Title_Tag" >
+                Icon
+                </div>
+                <div className = "Style_guide_Tag">
+                style guide
+                </div>
+            </div>
+            <div className = "Tag">
+            Basic Icon
+            </div>
+            <div className = "Icon">
+                <ul>
+                <li><ReactSVG src="/icon/activity.svg" /></li>
+                <li><ReactSVG src="/icon/airplay.svg" /></li>
+                <li><ReactSVG src="/icon/alert-circle.svg" /></li>
+                
+                </ul>
+           
+
+
+            </div>
         </div>
     )
 }
