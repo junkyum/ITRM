00760a1cdaec05318cd5766cc4521fc4455a9002
junkyum <junkyum884@naver.com>
2020-09-01 17:26:14 +0900

    jk 22

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 export { default as Main } from './Main';
 export { default as Color } from './Color';
 export { default as Typography } from './Typography';
-export { default as Icon} from './Icon';
-export { default as Logo} from './Logo';
+export { default as Icon } from './Icon';
+export { default as Logo } from './Logo';
+export { default as Buttons } from './Buttons';
