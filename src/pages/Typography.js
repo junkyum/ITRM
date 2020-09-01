4964b0bbb30d2351c0880a2b27599c9f747103a0
junkyum <junkyum884@naver.com>
2020-09-01 13:41:27 +0900

    jk

--- src/pages/Typography.js
+++ src/pages/Typography.js
@@ -39,15 +39,43 @@ const Typography = () => {
                 </div>
              <div className = "H_style">
                  <h1>h1. ITRM Heading</h1>
-
+                 <p>H1 bold / size 36px / line 48px / spacing -1.5px</p>
                  <h2>h2. ITRM Heading</h2>
+                 <p>H2 bold / size 28px / line 40px / spacing -1px</p>
                  <h3>h3. ITRM Heading</h3>
+                 <p>H3 bold / size 24px / line 34px / spacing -0.5px</p>
                  <h4>h4. ITRM Heading</h4>
+                 <p>H1 bold / size 18px / line 26px / spacing -0.5px</p>
                  <h5>h5. ITRM Heading</h5>
+                 <p>H1 bold / size 14px / line 18px / spacing 0px</p>
                  <h6>h6. ITRM Heading</h6>
+                 <p>H1 bold / size 12px / line 16px / spacing 0px</p>
+
              </div>
              <div className ="Text_style">
-                 
+                 <div className = "Text_style_left">
+                    <strong>
+                    Body Text
+                    </strong>
+                    <strong>
+                    Caption Text   
+                    </strong>
+                    <strong>
+                    Table Text    
+                    </strong>
+                    <strong>
+                    Information items 
+                    </strong>
+
+                 </div>
+                 <div className = "Text_style_right">
+                 <p>Our semiconductor solutions are the engines that power today’s devices.</p>
+                 <p>Body     size 14px  /  line 22px / spacing 0px</p>
+                 <p>State: Normal</p>
+                 <p>size 11px  /  line 16px / spacing 0px</p>
+                 <p>Network Device</p>
+                 <p>size 12px  /  line 18px / spacing 0px</p>
+                 </div>
              </div>    
             </div>
         </div>
