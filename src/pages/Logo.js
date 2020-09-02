cd8876209fdd397a5b515fb58f16c124efbdc3ad
greendp <greendp@naver.com>
2020-09-02 09:24:27 +0900

    welcome

--- src/pages/Logo.js
+++ src/pages/Logo.js
@@ -1,10 +1,26 @@
 import React from 'react';
-
+import '../css/Logo.css'
 
 const Logo = () => {
     return (
-        <div>
-            Logo
+        <div className = "Container">
+            <div className = "Tag">
+                <div className = "Title_Tag" >
+                Logo
+                </div>
+                <div className = "Style_guide_Tag">
+                STYLE GUIDE
+                </div>
+            </div>
+            <div className = "itrm-logo">
+                ITRM Logo
+            </div>
+            <div className = "logo-box">
+                <img src = "../../img/ITRM_logo.png"></img>
+            </div>
+            <div className = "logo-box-blue">
+                <img src = "../../img/ITRM_logo_w.png"></img>
+            </div>
         </div>
     )
 }
