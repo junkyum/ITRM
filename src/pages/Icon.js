72244d5c795b2410b4d387243f03d9d0036c7ec9
greendp <greendp@naver.com>
2020-09-02 18:01:09 +0900

    22

--- src/pages/Icon.js
+++ src/pages/Icon.js
@@ -7,21 +7,266 @@ const Icon = () => {
         <div className = "Container">
             <div className = "Tag">
                 <div className = "Title_Tag" >
-                Icon
+                    Icon
                 </div>
                 <div className = "Style_guide_Tag">
-                style guide
+                    style guide
                 </div>
             </div>
-            <div className = "Tag">
-            Basic Icon
+            <div className = "Primary-icon">
+                Basic Icon
             </div>
             <div className = "Icon">
                 <ul>
-                <li><ReactSVG src="/icon/activity.svg" /></li>
-                <li><ReactSVG src="/icon/airplay.svg" /></li>
-                <li><ReactSVG src="/icon/alert-circle.svg" /></li>
+                    <li><ReactSVG src="/icon/activity.svg" /></li>
+                    <li><ReactSVG src="/icon/airplay.svg" /></li>
+                    <li><ReactSVG src="/icon/alert-circle.svg" /></li>
+                    <li><ReactSVG src="/icon/alert-octagon.svg" /></li>
+                    <li><ReactSVG src="/icon/alert-triangle.svg" /></li>
+                    <li><ReactSVG src="/icon/align-center.svg" /></li>
+                    <li><ReactSVG src="/icon/align-justify.svg" /></li>
+                    <li><ReactSVG src="/icon/align-left.svg" /></li>
+                    <li><ReactSVG src="/icon/align-right.svg" /></li>
+                    <li><ReactSVG src="/icon/anchor.svg" /></li>
+                    <li><ReactSVG src="/icon/archive.svg" /></li>
+                    <li><ReactSVG src="/icon/arrow-down.svg" /></li>
+
+                    <li><ReactSVG src="/icon/arrow-down-circle.svg" /></li>
+                    <li><ReactSVG src="/icon/arrow-down-left.svg" /></li>
+                    <li><ReactSVG src="/icon/arrow-down-right.svg" /></li>
+                    <li><ReactSVG src="/icon/arrow-left.svg" /></li>
+                    <li><ReactSVG src="/icon/arrow-left-circle.svg" /></li>
+                    <li><ReactSVG src="/icon/arrow-right.svg" /></li>
+                    <li><ReactSVG src="/icon/arrow-right-circle.svg" /></li>
+                    <li><ReactSVG src="/icon/arrow-up.svg" /></li>
+                    <li><ReactSVG src="/icon/arrow-up-circle.svg" /></li>
+                    <li><ReactSVG src="/icon/arrow-up-left.svg" /></li>
+                    <li><ReactSVG src="/icon/arrow-up-right.svg" /></li>
+                    <li><ReactSVG src="/icon/at-sign.svg" /></li>
+
+                    <li><ReactSVG src="/icon/award.svg" /></li>
+                    <li><ReactSVG src="/icon/bar-chart.svg" /></li>
+                    <li><ReactSVG src="/icon/bar-chart-2.svg" /></li>
+                    <li><ReactSVG src="/icon/bell.svg" /></li>
+                    <li><ReactSVG src="/icon/bell-off.svg" /></li>
+                    <li><ReactSVG src="/icon/bluetooth.svg" /></li>
+                    <li><ReactSVG src="/icon/book.svg" /></li>
+                    <li><ReactSVG src="/icon/book-open.svg" /></li>
+                    <li><ReactSVG src="/icon/bookmark.svg" /></li>
+                    <li><ReactSVG src="/icon/box.svg" /></li>
+                    <li><ReactSVG src="/icon/briefcase.svg" /></li>
+                    <li><ReactSVG src="/icon/calendar.svg" /></li>
+                    
+                    <li><ReactSVG src="/icon/camera.svg" /></li>
+                    <li><ReactSVG src="/icon/camera-off.svg" /></li>
+                    <li><ReactSVG src="/icon/check.svg" /></li>
+                    <li><ReactSVG src="/icon/check-circle.svg" /></li>
+                    <li><ReactSVG src="/icon/check-square.svg" /></li>
+                    <li><ReactSVG src="/icon/chevron-down.svg" /></li>
+                    <li><ReactSVG src="/icon/chevron-left.svg" /></li>
+                    <li><ReactSVG src="/icon/chevron-right.svg" /></li>
+                    <li><ReactSVG src="/icon/chevron-up.svg" /></li>
+                    <li><ReactSVG src="/icon/chevrons-down.svg" /></li>
+                    <li><ReactSVG src="/icon/chevrons-left.svg" /></li>
+                    <li><ReactSVG src="/icon/chevrons-right.svg" /></li>
+
+                    <li><ReactSVG src="/icon/chevrons-up.svg" /></li>
+                    <li><ReactSVG src="/icon/circle.svg" /></li>
+                    <li><ReactSVG src="/icon/clipboard.svg" /></li>
+                    <li><ReactSVG src="/icon/clock.svg" /></li>
+                    <li><ReactSVG src="/icon/code.svg" /></li>
+                    <li><ReactSVG src="/icon/codepen.svg" /></li>
+                    <li><ReactSVG src="/icon/compass.svg" /></li>
+                    <li><ReactSVG src="/icon/copy.svg" /></li>
+                    <li><ReactSVG src="/icon/corner-down-left.svg" /></li>
+                    <li><ReactSVG src="/icon/corner-down-right.svg" /></li>
+                    <li><ReactSVG src="/icon/corner-left-down.svg" /></li>
+                    <li><ReactSVG src="/icon/corner-left-up.svg" /></li>
+
+                    <li><ReactSVG src="/icon/corner-right-down.svg" /></li>
+                    <li><ReactSVG src="/icon/corner-right-up.svg" /></li>
+                    <li><ReactSVG src="/icon/corner-up-left.svg" /></li>
+                    <li><ReactSVG src="/icon/corner-up-right.svg" /></li>
+                    <li><ReactSVG src="/icon/cpu.svg" /></li>
+                    <li><ReactSVG src="/icon/credit-card.svg" /></li>
+                    <li><ReactSVG src="/icon/crop.svg" /></li>
+                    <li><ReactSVG src="/icon/crosshair.svg" /></li>
+                    <li><ReactSVG src="/icon/database.svg" /></li>
+                    <li><ReactSVG src="/icon/delete.svg" /></li>
+                    <li><ReactSVG src="/icon/disc.svg" /></li>
+                    <li><ReactSVG src="/icon/download.svg" /></li>
+
+                    <li><ReactSVG src="/icon/download-cloud.svg" /></li>
+                    <li><ReactSVG src="/icon/droplet.svg" /></li>
+                    <li><ReactSVG src="/icon/edit.svg" /></li>
+                    <li><ReactSVG src="/icon/edit-2.svg" /></li>
+                    <li><ReactSVG src="/icon/edit-3.svg" /></li>
+                    <li><ReactSVG src="/icon/external-link.svg" /></li>
+                    <li><ReactSVG src="/icon/eye.svg" /></li>
+                    <li><ReactSVG src="/icon/eye-off.svg" /></li>
+                    <li><ReactSVG src="/icon/fast-forward.svg" /></li>
+                    <li><ReactSVG src="/icon/file.svg" /></li>
+                    <li><ReactSVG src="/icon/file-minus.svg" /></li>
+                    <li><ReactSVG src="/icon/file-plus.svg" /></li>
+
+                    <li><ReactSVG src="/icon/file-text.svg" /></li>
+                    <li><ReactSVG src="/icon/filter.svg" /></li>
+                    <li><ReactSVG src="/icon/flag.svg" /></li>
+                    <li><ReactSVG src="/icon/folder.svg" /></li>
+                    <li><ReactSVG src="/icon/folder-minus.svg" /></li>
+                    <li><ReactSVG src="/icon/folder-plus.svg" /></li>
+                    <li><ReactSVG src="/icon/git-branch.svg" /></li>
+                    <li><ReactSVG src="/icon/git-commit.svg" /></li>
+                    <li><ReactSVG src="/icon/git-merge.svg" /></li>
+                    <li><ReactSVG src="/icon/git-pull-request.svg" /></li>
+                    <li><ReactSVG src="/icon/globe.svg" /></li>
+                    <li><ReactSVG src="/icon/grid.svg" /></li>
+
+                    <li><ReactSVG src="/icon/hard-drive.svg" /></li>
+                    <li><ReactSVG src="/icon/hash.svg" /></li>
+                    <li><ReactSVG src="/icon/headphones.svg" /></li>
+                    <li><ReactSVG src="/icon/heart.svg" /></li>
+                    <li><ReactSVG src="/icon/help-circle.svg" /></li>
+                    <li><ReactSVG src="/icon/home.svg" /></li>
+                    <li><ReactSVG src="/icon/image.svg" /></li>
+                    <li><ReactSVG src="/icon/inbox.svg" /></li>
+                    <li><ReactSVG src="/icon/info.svg" /></li>
+                    <li><ReactSVG src="/icon/instagram.svg" /></li>
+                    <li><ReactSVG src="/icon/italic.svg" /></li>
+                    <li><ReactSVG src="/icon/layers.svg" /></li>
+
+                    <li><ReactSVG src="/icon/layout.svg" /></li>
+                    <li><ReactSVG src="/icon/life-buoy.svg" /></li>
+                    <li><ReactSVG src="/icon/link.svg" /></li>
+                    <li><ReactSVG src="/icon/link-2.svg" /></li>
+                    <li><ReactSVG src="/icon/list.svg" /></li>
+                    <li><ReactSVG src="/icon/loader.svg" /></li>
+                    <li><ReactSVG src="/icon/lock.svg" /></li>
+                    <li><ReactSVG src="/icon/log-in.svg" /></li>
+                    <li><ReactSVG src="/icon/log-out.svg" /></li>
+                    <li><ReactSVG src="/icon/mail.svg" /></li>
+                    <li><ReactSVG src="/icon/map.svg" /></li>
+                    <li><ReactSVG src="/icon/map-pin.svg" /></li>
+
+                    <li><ReactSVG src="/icon/maximize.svg" /></li>
+                    <li><ReactSVG src="/icon/maximize-2.svg" /></li>
+                    <li><ReactSVG src="/icon/menu.svg" /></li>
+                    <li><ReactSVG src="/icon/message-circle.svg" /></li>
+                    <li><ReactSVG src="/icon/message-square.svg" /></li>
+                    <li><ReactSVG src="/icon/mic.svg" /></li>
+                    <li><ReactSVG src="/icon/mic-off.svg" /></li>
+                    <li><ReactSVG src="/icon/minimize.svg" /></li>
+                    <li><ReactSVG src="/icon/minimize-2.svg" /></li>
+                    <li><ReactSVG src="/icon/minus.svg" /></li>
+                    <li><ReactSVG src="/icon/minus-circle.svg" /></li>
+                    <li><ReactSVG src="/icon/minus-square.svg" /></li>
+
+                    <li><ReactSVG src="/icon/monitor.svg" /></li>
+                    <li><ReactSVG src="/icon/more-horizontal.svg" /></li>
+                    <li><ReactSVG src="/icon/more-vertical.svg" /></li>
+                    <li><ReactSVG src="/icon/move.svg" /></li>
+                    <li><ReactSVG src="/icon/music.svg" /></li>
+                    <li><ReactSVG src="/icon/navigation.svg" /></li>
+                    <li><ReactSVG src="/icon/navigation-2.svg" /></li>
+                    <li><ReactSVG src="/icon/octagon.svg" /></li>
+                    <li><ReactSVG src="/icon/package.svg" /></li>
+                    <li><ReactSVG src="/icon/paperclip.svg" /></li>
+                    <li><ReactSVG src="/icon/pause.svg" /></li>
+                    <li><ReactSVG src="/icon/pause-circle.svg" /></li>
+
+                    <li><ReactSVG src="/icon/percent.svg" /></li>
+                    <li><ReactSVG src="/icon/phone.svg" /></li>
+                    <li><ReactSVG src="/icon/phone-call.svg" /></li>
+                    <li><ReactSVG src="/icon/phone-outgoing.svg" /></li>
+                    <li><ReactSVG src="/icon/pie-chart.svg" /></li>
+                    <li><ReactSVG src="/icon/play.svg" /></li>
+                    <li><ReactSVG src="/icon/play-circle.svg" /></li>
+                    <li><ReactSVG src="/icon/plus.svg" /></li>
+                    <li><ReactSVG src="/icon/plus-circle.svg" /></li>
+                    <li><ReactSVG src="/icon/plus-square.svg" /></li>
+                    <li><ReactSVG src="/icon/pocket.svg" /></li>
+                    <li><ReactSVG src="/icon/power.svg" /></li>
+
+                    <li><ReactSVG src="/icon/printer.svg" /></li>
+                    <li><ReactSVG src="/icon/radio.svg" /></li>
+                    <li><ReactSVG src="/icon/refresh-ccw.svg" /></li>
+                    <li><ReactSVG src="/icon/refresh-cw.svg" /></li>
+                    <li><ReactSVG src="/icon/repeat.svg" /></li>
+                    <li><ReactSVG src="/icon/rewind.svg" /></li>
+                    <li><ReactSVG src="/icon/rotate-ccw.svg" /></li>
+                    <li><ReactSVG src="/icon/rotate-cw.svg" /></li>
+                    <li><ReactSVG src="/icon/rss.svg" /></li>
+                    <li><ReactSVG src="/icon/save.svg" /></li>
+                    <li><ReactSVG src="/icon/scissors.svg" /></li>
+                    <li><ReactSVG src="/icon/search.svg" /></li>
+
+                    <li><ReactSVG src="/icon/send.svg" /></li>
+                    <li><ReactSVG src="/icon/server.svg" /></li>
+                    <li><ReactSVG src="/icon/settings.svg" /></li>
+                    <li><ReactSVG src="/icon/share.svg" /></li>
+                    <li><ReactSVG src="/icon/share-2.svg" /></li>
+                    <li><ReactSVG src="/icon/shield.svg" /></li>
+                    <li><ReactSVG src="/icon/shield-off.svg" /></li>
+                    <li><ReactSVG src="/icon/shopping-bag.svg" /></li>
+                    <li><ReactSVG src="/icon/shuffle.svg" /></li>
+                    <li><ReactSVG src="/icon/sidebar.svg" /></li>
+                    <li><ReactSVG src="/icon/skip-back.svg" /></li>
+                    <li><ReactSVG src="/icon/skip-forward.svg" /></li>
+
+                    <li><ReactSVG src="/icon/slash.svg" /></li>
+                    <li><ReactSVG src="/icon/sliders.svg" /></li>
+                    <li><ReactSVG src="/icon/smartphone.svg" /></li>
+                    <li><ReactSVG src="/icon/speaker.svg" /></li>
+                    <li><ReactSVG src="/icon/square.svg" /></li>
+                    <li><ReactSVG src="/icon/star.svg" /></li>
+                    <li><ReactSVG src="/icon/stop-circle.svg" /></li>
+                    <li><ReactSVG src="/icon/tablet.svg" /></li>
+                    <li><ReactSVG src="/icon/tag.svg" /></li>
+                    <li><ReactSVG src="/icon/terminal.svg" /></li>
+                    <li><ReactSVG src="/icon/thermometer.svg" /></li>
+                    <li><ReactSVG src="/icon/thumbs-down.svg" /></li>
+
+                    <li><ReactSVG src="/icon/thumbs-up.svg" /></li>
+                    <li><ReactSVG src="/icon/toggle-left.svg" /></li>
+                    <li><ReactSVG src="/icon/toggle-right.svg" /></li>
+                    <li><ReactSVG src="/icon/trash.svg" /></li>
+                    <li><ReactSVG src="/icon/trash-2.svg" /></li>
+                    <li><ReactSVG src="/icon/trending-down.svg" /></li>
+                    <li><ReactSVG src="/icon/trending-up.svg" /></li>
+                    <li><ReactSVG src="/icon/triangle.svg" /></li>
+                    <li><ReactSVG src="/icon/truck.svg" /></li>
+                    <li><ReactSVG src="/icon/type.svg" /></li>
+                    <li><ReactSVG src="/icon/underline.svg" /></li>
+                    <li><ReactSVG src="/icon/unlock.svg" /></li>
+
+                    <li><ReactSVG src="/icon/upload.svg" /></li>
+                    <li><ReactSVG src="/icon/upload-cloud.svg" /></li>
+                    <li><ReactSVG src="/icon/user.svg" /></li>
+                    <li><ReactSVG src="/icon/user-check.svg" /></li>
+                    <li><ReactSVG src="/icon/user-minus.svg" /></li>
+                    <li><ReactSVG src="/icon/user-plus.svg" /></li>
+                    <li><ReactSVG src="/icon/user-x.svg" /></li>
+                    <li><ReactSVG src="/icon/users.svg" /></li>
+                    <li><ReactSVG src="/icon/volume.svg" /></li>
+                    <li><ReactSVG src="/icon/volume-1.svg" /></li>
+                    <li><ReactSVG src="/icon/volume-2.svg" /></li>
+                    <li><ReactSVG src="/icon/volume-x.svg" /></li>
+
+                    <li><ReactSVG src="/icon/watch.svg" /></li>
+                    <li><ReactSVG src="/icon/wifi.svg" /></li>
+                    <li><ReactSVG src="/icon/wifi-off.svg" /></li>
+                    <li><ReactSVG src="/icon/x.svg" /></li>
+                    <li><ReactSVG src="/icon/x-circle.svg" /></li>
+                    <li><ReactSVG src="/icon/x-square.svg" /></li>
+                    <li><ReactSVG src="/icon/youtube.svg" /></li>
+                    <li><ReactSVG src="/icon/zap.svg" /></li>
+                    <li><ReactSVG src="/icon/zap-off.svg" /></li>
+                    <li><ReactSVG src="/icon/zoom-in.svg" /></li>
+                    <li><ReactSVG src="/icon/zoom-out.svg" /></li>
                 </ul>
+            </div>
+            <div className = "Secondary-icon">
+                Basic Icon
             </div>
         </div>
     )
