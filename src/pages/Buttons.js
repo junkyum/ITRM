fff78bddc96da484378ebfee849188d820031018
junkyum <junkyum884@naver.com>
2020-09-02 13:43:31 +0900

    jk 2e233

--- src/pages/Buttons.js
+++ src/pages/Buttons.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ReactSVG } from 'react-svg'
 import '../css/Buttons.css'
 
 const Buttons = () => {
@@ -47,32 +48,35 @@ const Buttons = () => {
                 </div>
             </div>
             <div className = "RadioBtn_ChkBox_Style">
+                <div className = "RadioBtn_ChkBox_Style_Tag">
+                    Radio Button & Check Box
+                </div>
                 <div className = "Radio_Style">
                     <div className ="Radio_Style_L">
                         <label>
-                        <input type="radio" value="option1" checked={true} />
+                        <input type="radio" value="option1" name ="NomalRadio" />
                         Nomal Radio Button
                         </label>
                         <label>
-                        <input type="radio" value="option1" checked={true} />
+                        <input type="radio" value="option1" name ="NomalRadio" />
                         Hover Radio Button
                         </label>
                         <label>
-                        <input type="radio" value="option1" checked={true} />
+                        <input type="radio" value="option1" name ="NomalRadio" />
                         Active Radio Button
                         </label>
                     </div>
                     <div className ="Radio_Style_R">
                         <label>
-                        <input type="radio" value="option1" checked={true} />
+                        <input type="radio" value="option1" name="SmallRadio" />
                         Small Radio Button
                         </label>
                         <label>
-                        <input type="radio" value="option1" checked={true} />
+                        <input type="radio" value="option1" name="SmallRadio" />
                         Hover Radio Button
                         </label>
                         <label>
-                        <input type="radio" value="option1" checked={true} />
+                        <input type="radio" value="option1" name="SmallRadio" />
                         Active Radio Button
                         </label>
                     </div>
@@ -80,36 +84,273 @@ const Buttons = () => {
                 <div className = "ChkBox_Style">
                      <div className ="ChkBox_Style_L">
                         <label>
-                        <input type="checkbox" value="option1" checked={true} />
+                        <input type="checkbox" value="option1"  />
                         Nomal Radio Button
                         </label>
                         <label>
-                        <input type="checkbox" value="option1" checked={true} />
+                        <input type="checkbox" value="option1"  />
                         Hover Radio Button
                         </label>
                         <label>
-                        <input type="checkbox" value="option1" checked={true} />
+                        <input type="checkbox" value="option1"  />
                         Active Radio Button
                         </label>
                     </div>
                     <div className ="ChkBox_Style_R">
                         <label>
-                        <input type="checkbox" value="option1" checked={true} />
+                        <input type="checkbox" value="option1"  />
                         Small Radio Button
                         </label>
                         <label>
-                        <input type="checkbox" value="option1" checked={true} />
+                        <input type="checkbox" value="option1"  />
                         Hover Radio Button
                         </label>
                         <label>
-                        <input type="checkbox" value="option1" checked={true} />
+                        <input type="checkbox" value="option1"  />
                         Active Radio Button
                         </label>
                     </div>
                 </div>
             </div>
             <div className = "FunctionBtn">
+                <div className = "Sort_Btn">
+                    <button >
+                    <ReactSVG 
+                    src="/icon/arrow-up.svg" 
+                    beforeInjection={(svg) => {
+                        svg.setAttribute("width", "14");
+                        svg.setAttribute("height", "14");
+                    }}
+                    />
+                    </button>
+                    <button >
+                    <ReactSVG 
+                    src="/icon/arrow-up.svg" 
+                    beforeInjection={(svg) => {
+                        svg.setAttribute("width", "14");
+                        svg.setAttribute("height", "14");
+                    }}
+                    />
+                    </button>
+                    <button >
+                    <ReactSVG 
+                    src="/icon/arrow-up.svg" 
+                    beforeInjection={(svg) => {
+                        svg.setAttribute("width", "14");
+                        svg.setAttribute("height", "14");
+                    }}
+                    />
+                    </button>
+                    <button >
+                    <ReactSVG 
+                    src="/icon/arrow-up.svg" 
+                    beforeInjection={(svg) => {
+                        svg.setAttribute("width", "14");
+                        svg.setAttribute("height", "14");
+                    }}
+                    />
+                    </button>
+                    <button >
+                    <ReactSVG 
+                    src="/icon/arrow-down.svg" 
+                    beforeInjection={(svg) => {
+                        svg.setAttribute("width", "14");
+                        svg.setAttribute("height", "14");
+                    }}
+                    />
+                    </button>
+                    <button >
+                    <ReactSVG 
+                    src="/icon/arrow-down.svg" 
+                    beforeInjection={(svg) => {
+                        svg.setAttribute("width", "14");
+                        svg.setAttribute("height", "14");
+                    }}
+                    />
+                    </button>
+                    <button >
+                    <ReactSVG 
+                    src="/icon/arrow-down.svg" 
+                    beforeInjection={(svg) => {
+                        svg.setAttribute("width", "14");
+                        svg.setAttribute("height", "14");
+                    }}
+                    />
+                    </button>
+                    <button >
+                    <ReactSVG 
+                    src="/icon/arrow-down.svg" 
+                    beforeInjection={(svg) => {
+                        svg.setAttribute("width", "14");
+                        svg.setAttribute("height", "14");
+                    }}
+                    />
+                    </button>
+                    <button >
+                    A
+                    <ReactSVG 
+                    src="/icon/arrow-down.svg" 
+                    beforeInjection={(svg) => {
+                        svg.setAttribute("width", "14");
+                        svg.setAttribute("height", "14");
+                    }}
+                    />
+                    </button>
+                    <button >
+                    A
+                    <ReactSVG 
+                    src="/icon/arrow-down.svg" 
+                    beforeInjection={(svg) => {
+                        svg.setAttribute("width", "14");
+                        svg.setAttribute("height", "14");
+                    }}
+                    />
+                    </button>
+                    <button >
+                    A
+                    <ReactSVG 
+                    src="/icon/arrow-down.svg" 
+                    beforeInjection={(svg) => {
+                        svg.setAttribute("width", "14");
+                        svg.setAttribute("height", "14");
+                    }}
+                    />
+                    </button>
+                    <button >
+                    A
+                    <ReactSVG 
+                    src="/icon/arrow-down.svg" 
+                    beforeInjection={(svg) => {
+                        svg.setAttribute("width", "14");
+                        svg.setAttribute("height", "14");
+                    }}
+                    />
+                    </button>
+                    <button >
+                    Z
+                    <ReactSVG 
+                    src="/icon/arrow-down.svg" 
+                    beforeInjection={(svg) => {
+                        svg.setAttribute("width", "14");
+                        svg.setAttribute("height", "14");
+                    }}
+                    />
+                    </button>
+                    <button >
+                    Z
+                    <ReactSVG 
+                    src="/icon/arrow-down.svg" 
+                    beforeInjection={(svg) => {
+                        svg.setAttribute("width", "14");
+                        svg.setAttribute("height", "14");
+                    }}
+                    />
+                    </button>
+                    <button >
+                    Z
+                    <ReactSVG 
+                    src="/icon/arrow-down.svg" 
+                    beforeInjection={(svg) => {
+                        svg.setAttribute("width", "14");
+                        svg.setAttribute("height", "14");
+                    }}
+                    />
+                    </button>
+                    <button >
+                    Z
+                    <ReactSVG 
+                    src="/icon/arrow-down.svg" 
+                    beforeInjection={(svg) => {
+                        svg.setAttribute("width", "14");
+                        svg.setAttribute("height", "14");
+                    }}
+                    />
+                    </button>
 
+                </div>
+
+                <div className = "Icon_Btn">
+                    <ul>
+                        <li><ReactSVG src="/icon/settings.svg" 
+                        beforeInjection={(svg) => {
+                            svg.setAttribute("width", "20");
+                            svg.setAttribute("height", "20");
+                        }}/>
+                        </li>
+                        <li><ReactSVG src="/icon/plus.svg" 
+                        beforeInjection={(svg) => {
+                            svg.setAttribute("width", "20");
+                            svg.setAttribute("height", "20");
+                        }}/>
+                        </li>
+                        <li><ReactSVG src="/icon/minus.svg" 
+                        beforeInjection={(svg) => {
+                            svg.setAttribute("width", "20");
+                            svg.setAttribute("height", "20");
+                        }}/>
+                        </li>
+                        <li><ReactSVG src="/icon/x.svg" 
+                        beforeInjection={(svg) => {
+                            svg.setAttribute("width", "20");
+                            svg.setAttribute("height", "20");
+                        }}/>
+                        </li>
+                        <li><ReactSVG src="/icon/log-out.svg" 
+                        beforeInjection={(svg) => {
+                            svg.setAttribute("width", "20");
+                            svg.setAttribute("height", "20");
+                        }}/>
+                        </li>
+                        <li><ReactSVG src="/icon/search.svg" 
+                        beforeInjection={(svg) => {
+                            svg.setAttribute("width", "20");
+                            svg.setAttribute("height", "20");
+                        }}/>
+                        </li>
+                        <li><ReactSVG src="/icon/info.svg" 
+                        beforeInjection={(svg) => {
+                            svg.setAttribute("width", "20");
+                            svg.setAttribute("height", "20");
+                        }}/>
+                        </li>
+                        <li><ReactSVG src="/icon/chevron-left.svg"
+                        beforeInjection={(svg) => {
+                            svg.setAttribute("width", "20");
+                            svg.setAttribute("height", "20");
+                        }}/>
+                        </li>
+                        <li><ReactSVG src="/icon/chevron-right.svg"
+                        beforeInjection={(svg) => {
+                            svg.setAttribute("width", "20");
+                            svg.setAttribute("height", "20");
+                        }}/>
+                        </li>
+                        <li><ReactSVG src="/icon/chevron-right.svg"
+                        beforeInjection={(svg) => {
+                            svg.setAttribute("width", "20");
+                            svg.setAttribute("height", "20");
+                        }}/>
+                        </li>
+                    </ul>
+                </div>
+                <div className = "Link_Btn">
+
+                </div>
+                <div className = "Function_Btn">
+                    
+                </div>
+                <div className = "Toggle">
+                    
+                </div>
+                <div className = "ListMore">
+                    
+                </div>
+                <div className = "Loader">
+                    
+                </div>
+                <div className = "Progress">
+                    
+                </div>
             </div>
         </div>
     )
